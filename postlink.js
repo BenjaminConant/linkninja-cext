25be094f2e710c87547f2b0f3d24b6b8106c23cb
Benjamin Conant <conantbenjamin@gmail.com>
2015-04-10 14:28:32 -0400

    my first commit

---
+++ postlink.js
@@ -0,0 +1,29 @@
+function postLink() {
+	var postTo = "http://localhost:9000/api/links"
+	var bkg = chrome.extension.getBackgroundPage();
+	var linkData = { _id: "5521951e74f83e6eab37ce6f"};
+	
+
+	chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs) {
+    	var url = tabs[0].url;
+    	linkData.url = url;
+    	bkg.console.log(url);
+
+    	$.ajax({
+	    	type: "POST",
+	    	url: postTo,
+	    	data: JSON.stringify(linkData),
+	    	contentType: "application/json; charset=utf-8",
+	    	dataType: "json",
+	    	success: function(data){alert(data);},
+	    	failure: function(errMsg) {
+	        	alert(errMsg);
+	    	}
+		});
+
+	});
+
+
+
+}
+chrome.browserAction.onClicked.addListener(postLink);
